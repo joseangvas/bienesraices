5a5d8fc7ddcf4b2dadb1d21f61f9ed5aa339ba55
joseangvas <developer.javp@gmail.com>
2023-07-14 07:13:03 -0400

    Inicio de Creación de Crud

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -5,10 +5,31 @@
   $db = conectarDB();
 
   if($_SERVER['REQUEST_METHOD'] === 'POST') {
-    echo "<pre>";
-    var_dump($_POST);
-    echo "</pre>";
-  }
+    // echo "<pre>";
+    // var_dump($_POST);
+    // echo "</pre>";
+
+    $titulo = $_POST['titulo'];
+    $precio = $_POST['precio'];
+    $descripcion = $_POST['descripcion'];
+    $habitaciones = $_POST['habitaciones'];
+    $wc = $_POST['wc'];
+    $estacionamiento = $_POST['estacionamiento'];
+    $vendedorId = $_POST['vendedor'];
+
+    // Insertar en la Base de Datos
+    $query = " INSERT INTO propiedades (titulo, precio, descripcion, habitaciones, wc, estacionamiento, vendedores_Id ) VALUES ( '$titulo', '$precio', '$descripcion', '$habitaciones', '$wc', '$estacionamiento', '$vendedorId' ) ";
+
+    // echo $query;
+
+    $resultado = mysqli_query($db, $query);
+
+    if($resultado) {
+      echo "Propiedad Insertada Correctamente";
+    } else {
+      echo "No se pudieron enviar Datos al Servidor";
+    };
+  };
   
   require '../../includes/funciones.php';
   incluirTemplate('header');
@@ -24,35 +45,35 @@
           <legend>Información General</legend>
 
           <label for="titulo">Titulo:</label>
-          <input type="text" id="titulo" placeholder="Titulo Propiedad">
+          <input type="text" id="titulo" name="titulo" placeholder="Titulo Propiedad">
 
           <label for="precio">Precio:</label>
-          <input type="number" id="precio" placeholder="Precio Propiedad">
+          <input type="number" id="precio" name="precio" placeholder="Precio Propiedad">
 
           <label for="imagen">Imagen:</label>
           <input type="file" id="imagen">
 
           <label for="descripcion">Descripción:</label>
-          <textarea type="text" id="descripcion"></textarea>
+          <textarea type="text" id="descripcion" name="descripcion"></textarea>
         </fieldset>
 
         <fieldset>
           <legend>Información de la Propiedad</legend>
 
           <label for="habitaciones">Habitaciones:</label>
-          <input type="number" id="habitaciones" placeholder="Ej: 3" min="1" max="9">
+          <input type="number" id="habitaciones" name="habitaciones" placeholder="N° Habitaciones" min="1" max="9">
 
           <label for="wc">Baños:</label>
-          <input type="number" id="wc" placeholder="Ej: 3" min="1" max="9">
+          <input type="number" id="wc" name="wc" placeholder="N° Baños" min="1" max="9">
 
           <label for="estacionamiento">Estacionamientos:</label>
-          <input type="number" id="estacionamiento" placeholder="Ej: 3" min="1" max="9">
+          <input type="number" id="estacionamiento" name="estacionamiento" placeholder="N° Estacionamientos" min="1" max="9">
         </fieldset>
 
         <fieldset>
           <legend>Vendedor</legend>
 
-          <select>
+          <select name="vendedor">
             <option value="1">Juan</option>
             <option value="2">Karen</option>
           </select>
